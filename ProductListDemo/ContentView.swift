ddede3ec477ba2e4739bc1368f1faf253272db57
Ivan Konukhov <ivan@MacBook-Pro-Ivan.local>
2024-08-06 18:18:11 +0400

    Initial project setup

---
+++ ProductListDemo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ProductListDemo
+//
+//  Created by Ivan Konukhov on 06.08.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProductListDemo/ProductListDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductListDemoApp.swift
+//  ProductListDemo
+//
+//  Created by Ivan Konukhov on 06.08.24.
+//
+
+import SwiftUI
+
+@main
+struct ProductListDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
